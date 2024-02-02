40c4a82f4e4e3431ea43749d101d1e03a8433a74
Timm Friebe <thekid@thekid.de>
2024-02-02 20:30:25 +0100

    Add test for match statement

--- src/test/php/lang/ast/syntax/php/unittest/IsOperatorTest.class.php
+++ src/test/php/lang/ast/syntax/php/unittest/IsOperatorTest.class.php
@@ -1,7 +1,7 @@
 <?php namespace lang\ast\syntax\php\unittest;
 
 use lang\ast\unittest\emit\EmittingTest;
-use test\{Assert, Test};
+use test\{Assert, Test, Values};
 
 class IsOperatorTest extends EmittingTest {
 
@@ -196,5 +196,19 @@ class IsOperatorTest extends EmittingTest {
     }');
 
     Assert::equals('string <Test>', $r);
+  }
+
+  #[Test, Values([[1, 'int'], ['test', 'string']])]
+  public function with_match_statement($arg, $expected) {
+    $r= $this->run('class <T> {
+      public function run(string|int $arg) {
+        return match {
+          $arg is string => "string",
+          $arg is int => "int",
+        };
+      }
+    }', $arg);
+
+    Assert::equals($expected, $r);
   }
 }
